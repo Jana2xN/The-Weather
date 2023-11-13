d0db37a461c744dc667d50fe6b74722575b59812
Jana2xN <jana.neirinck@hotmail.com>
2023-11-13 16:43:38 +0100

    Use an api to define the city geo-location data from the user-input

--- script.js
+++ script.js
@@ -6,22 +6,33 @@ document.addEventListener("DOMContentLoaded", function () {
     weatherForm.addEventListener("submit", function (event) {
         event.preventDefault();
         const city = cityInput.value;
-        getWeather(city)
+        getGeolocation(city)
             .then(data => {
-                weatherResult.textContent = `Weather in ${city}: ${data.weather}`;
+                const { lat, lng } = data.results[0].geometry;
+                weatherResult.textContent = `Geolocation for ${city}: Lat ${lat}, Lng ${lng}`;
             })
             .catch(error => {
                 weatherResult.textContent = `Error: ${error.message}`;
             });
     });
 
-    function getWeather(city) {
-        return fetch(`https://api.example.com/weather?city=${city}`)
-            .then(response => {
-                if (!response.ok) {
-                    throw new Error('Network response was not ok');
-                }
-                return response.json();
-            });
+    async function getGeolocation(city) {
+        const apiKey = 'YOUR_API_KEY'; // Replace with your API key
+        const url = `https://api.opencagedata.com/geocode/v1/json?q=${city}&key=${apiKey}`;
+        
+        const response = await fetch(url);
+
+        if (!response.ok) {
+            throw new Error('Network response was not ok');
+        }
+
+        const data = await response.json();
+
+        if (data.results.length === 0) {
+            throw new Error('Geolocation data not found');
+        }
+
+        return data;
     }
 });
+
