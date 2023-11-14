9c17e169f8fa18786fc1773d50eb9eb168c66264
Jana2xN <jana.neirinck@hotmail.com>
2023-11-14 12:26:21 +0100

    Manipulate your DOM in order to display the weather for the next 5 days in your application

--- script.js
+++ script.js
@@ -33,9 +33,40 @@ document.addEventListener("DOMContentLoaded", function () {
     }
 
     function generateWeatherHTML(data) {
-        // Your logic to format and display the weather data goes here
-        // Adjust this function based on the structure of the data from the API
-        const html = '<h2>Weather Data:</h2>'; // Add your HTML generation logic here
-        return html;
+        const forecast = data.list;
+    
+        // Clear previous results
+        weatherResult.innerHTML = '';
+    
+        // Iterate through the forecast data
+        for (let i = 0; i < forecast.length; i += 8) {
+            const dayData = forecast[i];
+            const date = new Date(dayData.dt * 1000);
+            const temperature = dayData.main.temp;
+            const description = dayData.weather[0].description;
+    
+            // Create elements to display the weather information
+            const dayElement = document.createElement('div');
+            dayElement.classList.add('weather-day');
+    
+            const dateElement = document.createElement('p');
+            dateElement.textContent = date.toDateString();
+    
+            const tempElement = document.createElement('p');
+            tempElement.textContent = `Temperature: ${temperature}°C`;
+    
+            const descriptionElement = document.createElement('p');
+            descriptionElement.textContent = `Description: ${description}`;
+    
+            // Append elements to the container
+            dayElement.appendChild(dateElement);
+            dayElement.appendChild(tempElement);
+            dayElement.appendChild(descriptionElement);
+    
+            weatherResult.appendChild(dayElement);
+        }
+    
+        // Make the weather result container visible
+        weatherResult.classList.remove("hidden");
     }
 });
