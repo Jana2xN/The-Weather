793adb751956e9e7069a961eb659451e295d519f
Jana2xN <jana.neirinck@hotmail.com>
2023-11-14 12:20:16 +0100

    Use an api to get the weather data for at least the next 5 days

--- script.js
+++ script.js
@@ -6,33 +6,36 @@ document.addEventListener("DOMContentLoaded", function () {
     weatherForm.addEventListener("submit", function (event) {
         event.preventDefault();
         const city = cityInput.value;
-        getGeolocation(city)
+
+        getWeatherData(city)
             .then(data => {
-                const { lat, lng } = data.results[0].geometry;
-                weatherResult.textContent = `Geolocation for ${city}: Lat ${lat}, Lng ${lng}`;
+                // Display the weather data
+                weatherResult.innerHTML = generateWeatherHTML(data);
+                weatherResult.classList.remove("hidden");
             })
             .catch(error => {
                 weatherResult.textContent = `Error: ${error.message}`;
+                weatherResult.classList.remove("hidden");
             });
     });
 
-    async function getGeolocation(city) {
-        const apiKey = 'YOUR_API_KEY'; // Replace with your API key
-        const url = `https://api.opencagedata.com/geocode/v1/json?q=${city}&key=${apiKey}`;
-        
+    async function getWeatherData(city) {
+        const apiKey = 'ed2b967e9fc91f60717efeeb95b6a75e'; // Replace with your OpenWeatherMap API key
+        const url = `https://api.openweathermap.org/data/2.5/forecast?q=${city}&appid=${apiKey}&units=metric`;
+    
         const response = await fetch(url);
-
+    
         if (!response.ok) {
             throw new Error('Network response was not ok');
         }
+    
+        return response.json();
+    }
 
-        const data = await response.json();
-
-        if (data.results.length === 0) {
-            throw new Error('Geolocation data not found');
-        }
-
-        return data;
+    function generateWeatherHTML(data) {
+        // Your logic to format and display the weather data goes here
+        // Adjust this function based on the structure of the data from the API
+        const html = '<h2>Weather Data:</h2>'; // Add your HTML generation logic here
+        return html;
     }
 });
-
